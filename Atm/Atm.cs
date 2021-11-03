030d4407688ebf29e86fc764136942901c566f76
lex_pron <pronyushkin.a.a@yandex.ru>
2021-11-03 22:58:36 +0300

    Initial commit

---
+++ Atm/Atm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+
+namespace Atm
+{
+    public class Atm
+    {
+        public void getAnswer(int cash, int[] banknote, int ind, string str, List<string> ans)
+        {
+            for(int i = ind; i > -1; i--)
+            {
+                if (cash - banknote[i] > 0)
+                {
+                    var tmp = banknote[i].ToString();
+                    str += tmp + " ";
+                    ind = i;
+                    getAnswer(cash - banknote[i], banknote, ind, str, ans);
+                    str = str.Substring(0, str.Length - tmp.Length - 1);
+                } else if (cash - banknote[i] == 0)
+                {
+                    str += banknote[i].ToString();
+                    ans.Add(str);
+                }
+            }
+        }
+    }
+}
---
+++ Atm/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atm
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount of money: ");
+            int cash = 0;
+            try
+            {
+                cash = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("This is not an integer!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("You entered too many numbers!");
+            }
+
+            if (cash < 0)
+            {
+                Console.WriteLine("Type a positive number!");
+            }
+
+            Console.WriteLine("Enter array length: ");
+            int size = 0;
+            try
+            {
+                size = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("This is not an integer!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("You entered too many numbers!");
+            }
+
+            if (size < 0)
+            {
+                Console.WriteLine("Type a positive number!");
+            }
+            
+            int[] banknote = new int[size];
+            Console.WriteLine("Enter array elements: ");
+            for (int i = 0; i < size; i++)
+            {
+                banknote[i] = 0;
+                try
+                {
+                    banknote[i] = Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("This is not an integer!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("You entered too many numbers!");
+                }
+                if (banknote[i] < 0)
+                {
+                    Console.WriteLine("Type a positive number!");
+                }
+            }
+
+            Array.Sort(banknote);
+            Atm atm = new Atm();
+            List<string> ans = new List<string>();
+            string str = "";
+            atm.getAnswer(cash, banknote, size - 1, str, ans);
+            for (int i = 0; i < ans.Count; i++)
+            {
+                Console.Write(ans[i]);
+                Console.WriteLine("\n");
+            }
+
+            Console.Write("{0} -- total number of options!", ans.Count);
+            Console.ReadKey();
+        }
+    }
+}
